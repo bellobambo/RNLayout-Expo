f358afd21aa48d100b25cdbe75652338081d3ff7
bellobambo <bellobambo21@gmail.com>
2024-09-07 15:46:53 +0100

    style

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import Box from "./components/Box";
 
 export default function App() {
   return (
+    // <View style={{ backgroundColor: "plum", flex: 1 }}></View>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Box style={{ backgroundColor: "purple" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "plum" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "lightblue" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "lightgreen" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "crimson" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "orange" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "blue" }}>Box 7</Box>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap-reverse",
+    height: 300,
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red",
   },
 });
