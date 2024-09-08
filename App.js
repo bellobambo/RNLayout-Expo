1578ce9c1480d70e376a331146388b05f9c29264
bellobambo <bellobambo21@gmail.com>
2024-09-08 18:49:01 +0100

    style

--- App.js
+++ App.js
@@ -8,7 +8,13 @@ export default function App() {
       <Box style={{ backgroundColor: "purple" }}>Box 1</Box>
       <Box style={{ backgroundColor: "plum" }}>Box 2</Box>
       <Box style={{ backgroundColor: "lightblue" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "lightgreen" }}>Box 4</Box>
+      <Box
+        style={{
+          backgroundColor: "lightgreen",
+        }}
+      >
+        Box 4
+      </Box>
       <Box style={{ backgroundColor: "crimson" }}>Box 5</Box>
       <Box style={{ backgroundColor: "orange" }}>Box 6</Box>
       <Box style={{ backgroundColor: "blue" }}>Box 7</Box>
@@ -18,10 +24,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    // flex: 1,
-    flexDirection: "row",
-    flexWrap: "wrap-reverse",
-    height: 300,
+    flex: 1,
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
